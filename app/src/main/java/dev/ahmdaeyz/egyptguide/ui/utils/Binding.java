ae5bee0399c30c4934ac101ddefd98f33ebcb6cc
ahmdaeyz <ahmdaeyz@gmail.com>
2020-05-25 19:28:19 +0200

    Support resource ids instead of urls and remove unused method

--- app/src/main/java/dev/ahmdaeyz/egyptguide/ui/utils/Binding.java
+++ app/src/main/java/dev/ahmdaeyz/egyptguide/ui/utils/Binding.java
@@ -5,27 +5,18 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.Request;
 import com.bumptech.glide.request.RequestOptions;
 
 import java.util.Optional;
 
-import jp.wasabeef.glide.transformations.CropCircleWithBorderTransformation;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class Binding {
 
-    public static void bindImageViewWithRoundedCorners(@NonNull ImageView imageView,@NonNull String url, Optional<Integer> radius) {
+    public static void bindImageViewWithRoundedCorners(@NonNull ImageView imageView, @NonNull Integer resId, Optional<Integer> radius) {
         Glide.with(imageView)
-                .load(url)
+                .load(resId)
                 .apply(RequestOptions.bitmapTransform(new RoundedCornersTransformation(radius.orElse(20),0, RoundedCornersTransformation.CornerType.ALL)))
-                .into(imageView);
-    }
-
-    public static void bindCircleImageView(@NonNull ImageView imageView,@NonNull String url,int color, int borderSize){
-        Glide.with(imageView)
-                .load(url)
-                .apply(RequestOptions.bitmapTransform(new CropCircleWithBorderTransformation(borderSize,color)))
                 .into(imageView);
     }
 }
